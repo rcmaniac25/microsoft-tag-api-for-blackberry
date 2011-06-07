79a2911f2ba6f97416c0849fcf38b8b66c2b7f25
rcmaniac25 <devnull@localhost>
2011-06-07 01:56:13 -0400

    Initial Commit

---
+++ src/mstag/Tag.java
@@ -0,0 +1,26 @@
+/**
+ * This software is distributed under Microsoft Public License (MSPL)
+ * see http://opensource.org/licenses/ms-pl.html
+ * 
+ * @author Vincent Simonetti
+ */
+package mstag;
+
+/**
+ * Base class for all Tag types.
+ */
+public class Tag
+{
+	/**
+	 * Initializes a new instance of the Tag class
+	 */
+	protected Tag()
+	{
+	}
+	
+	//TODO:
+	//String: AssociatedPayload, InteractionNote, Title
+	//TagStatus (to implement): Status
+	//ImageTypes (to implement): Types
+	//Calendar: UTCEndDate, UTCStartDate
+}
---
+++ src/mstag/UserCredential.java
@@ -0,0 +1,15 @@
+/**
+ * This software is distributed under Microsoft Public License (MSPL)
+ * see http://opensource.org/licenses/ms-pl.html
+ * 
+ * @author Vincent Simonetti
+ */
+package mstag;
+
+/**
+ * Holds user credential data.
+ */
+public class UserCredential
+{
+	//TODO
+}
